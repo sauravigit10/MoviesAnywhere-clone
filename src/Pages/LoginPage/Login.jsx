3f16a96aa93db2ca2520e18cdece9310eb8e2657
sauravigit10 <sauravichapagain98@gmail.com>
2026-03-31 22:12:07 +0545

    responsive along with form validation

--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -1,59 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+ 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+ 
+  const validate = () => {
+    let newErrors = {};
+
+    
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+    }
+
+    if (!formData.password) {
+      newErrors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Password must be at least 6 characters";
+    }
+
+    return newErrors;
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validationErrors = validate();
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      console.log("Form submitted:", formData);
+      alert("Login successful!");
+    }
+  };
+
   return (
     <div>
       <Navbar />
+
       <div className="LoginDiv">
-        <div className="LoginContent">
+        <form className="LoginContent" onSubmit={handleSubmit}>
           <div className="LoginContent2">
+
             <div className="div1">
               <h4 className="LoginHeading">Welcome Back!</h4>
               <h3 className="LoginSubheading">Sign in using</h3>
             </div>
+
+         
             <div className="div1">
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" />
+              <label>Email:</label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              {errors.email && (
+                <p style={{ color: "red", fontSize: "12px" }}>
+                  {errors.email}
+                </p>
+              )}
             </div>
+
+         
             <div className="div1">
-              <label htmlFor="password">Password:</label>
-              <input type="password" id="password" name="password" />
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+              {errors.password && (
+                <p style={{ color: "red", fontSize: "12px" }}>
+                  {errors.password}
+                </p>
+              )}
             </div>
+
             <div className="remeber">
-              <div
-                style={{
-                  display: "flex",
-                  width: "10vw",
-                  flexDirection: "row",
-                  margin: "0",
-                  alignItems: "center",
-                }}
-              >
-                <input type="checkbox" style={{}} />
-                <span style={{ color: "black", width: "40vw" }}>
-                  Remember me
-                </span>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <input type="checkbox" />
+                <span style={{ color: "black" }}>Remember me</span>
               </div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  fontSize: "14px",
-                  justifyContent: "center",
-                }}
-              >
-                <a className="link" href="/signup">
-                  Reset Password
-                </a>
-              </div>
+
+              <a className="link" href="/signup">
+                Reset Password
+              </a>
             </div>
-            <button className="LoginButton">Continue</button>
+
+            <button className="LoginButton" type="submit">
+              Continue
+            </button>
+
             <hr style={{ border: "1px solid grey", width: "95%" }} />
-            <button className="LoginButton">Continue with Google</button>
-            <button className="LoginButton">Continue with Apple</button>
+
+            <button className="LoginButton" type="button">
+              Continue with Google
+            </button>
+            <button className="LoginButton" type="button">
+              Continue with Apple
+            </button>
+
             <div>
               <span style={{ color: "black" }}>
                 New Here?{" "}
@@ -65,11 +132,14 @@ function Login() {
                 </a>
               </span>
             </div>
+
           </div>
-        </div>
+        </form>
       </div>
-      <Footer/> 
+
+      <Footer />
     </div>
   );
 }
+
 export default Login;
